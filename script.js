ae502d3d9892088c5db4fe73e17807ffa7e91107
Wenderson Damazio <wendersonmendes60@gmail.com>
2023-01-03 15:19:55 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var nome_carro = "Novo-Supra";
+var n_portas = 2;
+var car_carro = "branco";
+var Supra_carro = 2022;
+var sistema_eletrrico = true;
+var velocidade = 0;
+var ligado = false;
+
+const marca = "Toyota";
+
+function acelerar(){
+    if(velocidade < 120){
+        velocidade +=1;
+    }
+}
+function freiar(){
+    if(velocidade > 0){
+        velocidade -=1;
+    }
+}
+function parar(){
+    if(velocidad = 0){
+        
+    }
+}
+function ligar(){
+    ligado = true
+}
+function desligar(){
+    ligado = false;
+}
