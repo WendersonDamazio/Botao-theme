25baf8e9ff7a7e34e08da8dda6f0aafd9bcaa286
Wenderson Damazio <wendersonmendes60@gmail.com>
2023-01-03 17:00:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,16 @@
-var nome_carro = "Novo-Supra";
-var n_portas = 2;
-var car_carro = "branco";
-var Supra_carro = 2022;
-var sistema_eletrrico = true;
-var velocidade = 0;
-var ligado = false;
-var parado = false;
+var button = document.getElementById("button");
+var dark_theme = true;
 
-const marca = "Toyota";
-
-function acelerar(){
-    if(velocidade < 120){
-        velocidade +=1;
-    }
-}
-function freiar(){
-    if(velocidade > 0){
-        velocidade -=1;
-    }
-}
-function parar(){
-    if(velocidad == 0){
-        parado = true;
+function change_background(){
+    if(dark_theme == false){
+        dark_theme = true
+        document.body.style.background = "red";
+        button.style.background = "black"
+        button.style.color ="red"
     }else{
-        parado = false;
-    }
-}
-function ligar(){
-    ligado = true
-    if(ligado == false){
-        ligar = true;
-    }
-}
-function desligar(){
-    if(ligado == true){
-        ligado = false;
+        dark_theme = false
+        document.body.style.background = "black";
+        button.style.background = "red"
+        button.style.color ="black"
     }
 }
